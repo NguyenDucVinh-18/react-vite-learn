1c282999b12c85ed205e0e5818e95b4eb2e0a440
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-03-17 17:19:44 +0700

    delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,39 @@ import TodoData from "./components/todo/TodoData";
 import "./components/todo/todo.css";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learn React" },
-    { id: 2, name: "Learn Vite" },
-  ]);
+  const [todoList, setTodoList] = useState([]);
 
   const addTodoList = (name) => {
     const newTodoList = { id: todoList.length + 1, name };
     setTodoList([...todoList, newTodoList]);
   };
 
+  const deleteTodo = (id) => {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addTodoList={addTodoList} />
-      <TodoData todoList={todoList} />
-      <div className="todo-image">
-        <img className="logo" src={reactLogo} />
-      </div>
+
+      {todoList.length > 0 ? (
+        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+      ) : (
+        <div className="todo-image">
+          <img className="logo" src={reactLogo} />
+        </div>
+      )}
+
+      {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
+
+
+      {todoList.length === 0 && (
+        <div className="todo-image">
+          <img className="logo" src={reactLogo} />
+        </div>
+      )} */}
     </div>
   );
 }
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,13 +1,16 @@
 const TodoData = (props) => {
-  const { todoList } = props;
+  const { todoList , deleteTodo} = props;
+  
   return (
     <div className="todo-data">
       <div>
         {todoList.map((item, index) => {
           return (
-            <div className="todo-item">
+            <div className="todo-item" key={item.id}>
               <div>{item.name}</div>
-              <button>Delete</button>
+              <button
+                onClick={() => deleteTodo(item.id)}
+              >Delete</button>
             </div>
           );
         })}
